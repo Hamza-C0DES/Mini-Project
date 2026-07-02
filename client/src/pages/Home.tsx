b24f8a7e9c65bb2668fe3b726c177ba8d8b93df0
garvin <garvinromelus@outlook.com>
2026-07-02 10:42:02 -0400

    created basic hook form that prints inputs into console

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonInput,
+  IonButton } from '@ionic/react';
+
+import { useForm } from "react-hook-form";
+//import GameForm from './components/GameForm';
 
 const Home: React.FC = () => {
+  const { register, handleSubmit, reset} = useForm();
+  const onSubmit = (data: any) =>{
+    console.log(data)
+
+  };
+  
   return (
     <IonPage>
       <IonHeader>
@@ -9,7 +25,25 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <p>Hello, world!</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <IonInput 
+          label="Room Code" 
+          {...register("roomCode", {required: true})}
+          type='number'
+          fill='outline'
+          />
+          <IonInput 
+          label="User"
+          {...register("userName", {required: true})}
+          fill='outline'
+          />
+          <IonInput 
+          label="Celebrity Name" 
+          {...register("celebName", {required: true})}
+          fill='outline'
+          />
+          <IonButton type="submit">Submit!</IonButton>
+        </form>
       </IonContent>
     </IonPage>
   );
